5c224addb3ca8698f14df05467236befde345dd8
jahanpd <jahan.pd@gmail.com>
2019-08-13 20:12:35 +1000

    pivoted to CNN classifier with normal vs abnormal CXRs

--- algorithms/autoencoder.py
+++ algorithms/autoencoder.py
@@ -8,38 +8,55 @@ from sklearn.metrics import roc_auc_score
 from sklearn.metrics import classification_report
 from pathlib import Path
 
-class convAutoencoder:
-    def __init__(self, save_path=None, data_path=None):
-        K.set_floatx('float16')
+tf.enable_eager_execution()
+
+class convNN:
+    def __init__(self, image_paths, labels, save_path=None):
+        K.set_floatx('float32')
+        self.image_paths = image_paths
+        self.labels = labels
+
         if save_path is not None:
             self.save_path = save_path
-        if data_path is not None:
-            self.data = data_path
-
-        if data_path is None:
-            print("need to add location of data")
 
         self.img_rows = 256
         self.img_cols = 256
         self.channels = 1
-        self.output = 256*256
-        self.cluster = 1
 
-    def _prep_extraction(self):
-        self.filenames = []
-        for filename in Path(self.data).glob('**/*.npz'):
-                self.filenames.append(str(filename))
-        self.indexes =(np.arange(len(self.filenames))).reshape((-1,self.cluster))
-        np.random.shuffle(self.indexes)
+        self.AUTOTUNE = tf.data.experimental.AUTOTUNE
+        self.optimizer = 'Adam'
+        self.loss = 'binary_crossentropy'
+        self.metrics = [tf.keras.metrics.AUC(),
+                        tf.keras.metrics.FalseNegatives(),
+                        tf.keras.metrics.FalsePositives(),
+                        tf.keras.metrics.TrueNegatives(),
+                        tf.keras.metrics.TruePositives()]
 
-    def _batch_data(self, iteration):
-        x = np.array(())
-        for n in range(self.cluster):
-            data = np.load(self.filenames[self.indexes[iteration][n]])
-            temp_x = data['a']
-            _, heightx, widthx = temp_x.shape
-            x = np.append(x, temp_x)
-        return x.reshape((-1,heightx,widthx,1))/255
+    def _preprocess_image(self, image):
+        image = tf.image.decode_jpeg(image, channels = self.channels)
+        image = tf.image.resize(image, [self.img_rows, self.img_cols])
+        image /= 255.0  # normalize to [0,1] range
+        return image
+
+    def _load_and_preprocess_image(self, path): # load from path and return tensor
+        image = tf.io.read_file(path)
+        return self._preprocess_image(image)
+
+
+
+    def build_dataset(self, batch_size=None):
+        path_ds = tf.data.Dataset.from_tensor_slices(self.image_paths)
+        image_ds = path_ds.map(self._load_and_preprocess_image,
+                                num_parallel_calls=self.AUTOTUNE)
+        label_ds = tf.data.Dataset.from_tensor_slices(tf.cast(self.labels, tf.int16))
+        image_label_ds = tf.data.Dataset.zip((image_ds, label_ds))
+        self.ds = image_label_ds.shuffle(buffer_size=2000)
+        if batch_size is not None:
+            self.ds = self.ds.batch(batch_size)
+        else:
+            self.ds = self.ds.batch(32)
+        self.ds = self.ds.repeat()
+        self.ds = self.ds.prefetch(buffer_size=self.AUTOTUNE)
 
     def identity_block(self,f, k, filters, stage, block):
         # defining name basis
@@ -133,82 +150,34 @@ class convAutoencoder:
 
 
     def build(self, shape=None):
-
-        self.shape = (256,256,1)
+        self.shape = (self.img_rows,self.img_cols,self.channels)
         if shape is not None:
             self.shape = shape
 
         # build network
-        inputs = Input(shape=self.shape)
+        input = Input(shape=self.shape)
 
-        ## ENCODE
-        f = Conv2D(filters = 64,
+        f = Conv2D(filters = 16,
             kernel_size = (5, 5),
             strides = (1,1), padding = 'same',
             name = 'conv1',
-            kernel_initializer = glorot_uniform(seed=0))(inputs)
+            kernel_initializer = glorot_uniform(seed=0))(input)
         f = BatchNormalization(axis = 3, name = '12c')(f)
         f = Activation('relu')(f)
-        f = MaxPooling2D((3,3),strides=(2,2))(f)
 
-        f = Conv2D(filters = 128,
-            kernel_size = (5, 5),
-            strides = (1,1), padding = 'same',
-            name = 'conv2',
-            kernel_initializer = glorot_uniform(seed=0))(f)
-        f = BatchNormalization(axis = 3, name = '22c')(f)
-        f = Activation('relu')(f)
-        f = MaxPooling2D((3,3),strides=(2,2))(f)
+        f = self.convolutional_block(f, k=3, filters=[16,16,32], stage='2', block='a', s=2)
+        f = self.identity_block(f, k=3, filters=[32,32,32], stage='2', block='b')
+        f = self.identity_block(f, k=3, filters=[32,32,32], stage='2', block='c')
+        f = GlobalAveragePooling2D()(f)
 
-        f = Conv2D(filters = 256,
-            kernel_size = (5, 5),
-            strides = (1,1), padding = 'same',
-            name = 'conv3',
-            kernel_initializer = glorot_uniform(seed=0))(f)
-        f = BatchNormalization(axis = 3, name = '32c')(f)
-        f = Activation('relu')(f)
-        f = MaxPooling2D((3,3),strides=(2,2))(f)
+        output = Dense(2,activation="softmax")(f)
 
-        f = Conv2D(filters = 512,
-            kernel_size = (5, 5),
-            strides = (1,1), padding = 'same',
-            name = 'conv4',
-            kernel_initializer = glorot_uniform(seed=0))(f)
-        f = BatchNormalization(axis = 3, name = '42c')(f)
-        f = Activation('relu')(f)
-        f = MaxPooling2D((3,3),strides=(2,2))(f)
+        self.nn = Model(inputs = input, outputs = output)
+        self.nn.compile(optimizer = self.optimizer,loss=self.loss, metrics=self.metrics)
+        self.nn.summary()
 
-        #f = GlobalAveragePooling2D()(f)
-
-        ## DECODE
-        # Stage 4 (≈6 lines)
-        f = Conv2D(512, (4, 4), strides=(2, 2), name='conv5', padding='same', kernel_initializer=glorot_uniform(seed=0))(f)
-        f = BatchNormalization(axis=3, name='bn_conv0')(f)
-        f = Activation('relu')(f)
-        f = UpSampling2D((2, 2))(f)
-
-        f = Conv2D(256, (4, 4), strides=(1, 1), name='conv6', padding='same', kernel_initializer=glorot_uniform(seed=0))(f)
-        f = BatchNormalization(axis=3, name='bn_conv')(f)
-        f = Activation('relu')(f)
-        f = UpSampling2D((2, 2))(f)
-
-        f = Conv2D(128, (1, 1), activation='sigmoid', padding='same', name='fc1024', kernel_initializer = glorot_uniform(seed=0))(f)
-        output = Flatten()(f)
-
-        self.autoencoder = Model(inputs = inputs, outputs=output, name='ResNetAE')
-        self.autoencoder.compile(optimizer='Adagrad', loss='mae')
-        self.autoencoder.summary()
-
-    def train(self, epochs, load=None):
-        # prep data extraction file locations
-        self._prep_extraction()
-        batch_n, _ = self.indexes.shape
-
+    def train(self, epoch, load=None):
         if load is not None:
-            self.autoencoder.load_weights(self.save_path)
-
-        for yolo in range(epochs):
-            for batch in range(batch_n):
-                x_train = self._batch_data(batch)
-                self.autoencoder.fit(x_train,x_train.reshape((-1,256*256)),epochs=100,batch_size=32)
-                self.autoencoder.save_weights(self.save_path)
+            self.nn.load_weights(self.save_path)
+        self.nn.fit(self.ds,epochs=epoch,steps_per_epoch=5000)
+        self.nn.save_weights(self.save_path)
